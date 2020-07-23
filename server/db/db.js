32015737921dd1dab99d4908bb922f2f475871e7
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-23 02:38:14 -0400

    CREATED Album, Artist, Song models with proper associations

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/juke', {
-  logging: false
-})
+// const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/juke', {
+//   logging: false
+// })
+const db = new Sequelize('juke', 'postgres', 'jh810506', {
+  dialect: 'postgres',
+  logging: false,
+});
 
-module.exports = db
+module.exports = db;
--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,19 @@
-const db = require('./db')
 // require each of your models here...
+const db = require('./db');
+const Album = require('./album');
+const Artist = require('./artist');
+const Song = require('./song');
 
 // ...and give them some nice associations here!
+Artist.hasMany(Album);
+Album.belongsTo(Artist);// artistId
+Artist.hasMany(Song);
+Song.belongsTo(Artist); // artistId
+Album.hasMany(Song);    
+Song.belongsTo(Album);  // albumId
 
 module.exports = {
-  db,
+  db, Album, Artist, Song
   // Include your models in your module.exports as well!
   // The seed file expects to find them there!
 }

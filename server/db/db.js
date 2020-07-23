6008c9b0e94519480e41b543ea89e4d150dd4665
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-23 15:33:53 -0400

    CREATED Album, Artist, Song models with proper associations

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,6 @@
 const Sequelize = require('sequelize')
-// const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/juke', {
-//   logging: false
-// })
-const db = new Sequelize('juke', 'postgres', 'jh810506', {
-  dialect: 'postgres',
-  logging: false,
-});
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/juke', {
+  logging: false
+})
 
 module.exports = db;

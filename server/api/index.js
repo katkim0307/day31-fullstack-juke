cd62920caaf3049e4b90e34f7de13acbe8c6944a
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-23 11:20:50 -0400

    ADDED /api/albums and /api/albums/albumId routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,26 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const { Album, Artist, Song } = require('../db');
 
 // connect your API routes here!
+// ALBUMS PAGE
+router.get('/albums', async (req, res, next) => {
+    try {
+        const allAlbums = await Album.findAll();
+        res.send(allAlbums);
+    } catch (err) { next(err) }
+});
+
+// INDIVIDUAL ALBUM PAGE BY ALBUM ID - Eager Loading
+router.get('/albums/:albumId', async (req, res, next) => {
+    try {
+        const album = await Album.findOne({
+            where: {
+                id: req.params.albumId,
+            },
+            include: Song,
+        });
+        res.send(album);
+    } catch (err) { next(err) }
+});
 
 module.exports = router
